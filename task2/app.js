3c002537234d0f68489da52502e073371640f0a0
Nikolay <pak_kolya98@mail.ru>
2017-11-19 12:09:06 +0300

    Update app.js

--- task2/app.js
+++ task2/app.js
@@ -1,7 +1,7 @@
 $(function(){
-				$("#root").append('<ul></ul>');
-        $("ul").append('<li></li>');
-        $("li").append('<span> Сделать задание #3 по web-программированию </span>');
+				$("#root").append('<ul>');
+        $("ul").append('<li>');
+        $("li").append('<span>Сделать задание #3 по web-программированию</span>');
 	      var r= $('<input id = "main" type="button" value="Удалить"/>');
         $("li").append(r);
         $("#main").click(function(){
