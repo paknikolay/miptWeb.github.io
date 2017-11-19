1e5d58b643136797ea366eff4ed247b72b3256e5
Nikolay <pak_kolya98@mail.ru>
2017-11-19 12:15:41 +0300

    Update app.js

--- task2/app.js
+++ task2/app.js
@@ -6,4 +6,16 @@ $(function(){
        $("#main").click(function(){
         	$(this).parent().remove()
         });
+
+       $("#root").append('<input id="add_task_input"> <button id="add_task"> Добавить </button>');
+       $("#add_task").click(function(){
+          var element = $('<li> <span></span> <button class="removing"> Удалить </button> </li>');
+          $('span', element).text( $("#add_task_input").val());
+          $("ul").append(element);
+          $(".removing").click(function(){
+        	$(this).parent().remove()
+        });
+
+
+        });
 })
