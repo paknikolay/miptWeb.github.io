247e196ddbbe0f2f6207e29517d43662eeb4a5a9
pak_kolya98@mail.ru <pak_kolya98@mail.ru>
2017-11-19 11:59:34 +0300

    task2 one more change

--- task2/app.js
+++ task2/app.js
@@ -2,18 +2,21 @@ $(function(){
 				$("#root").append('<ul></ul>');
         $("ul").append('<li></li>');
         $("li").append('<span> Сделать задание #3 по web-программированию </span>');
-        $("").append($('<input id = "main" type="button" value="Удалить"/>'));
+	      var r= $('<input id = "main" type="button" value="Удалить"/>');
+        $("li").append(r);
         $("#main").click(function(){
         	$(this).parent().remove()
         });
 
-        $("ul").append('<li><input id="add_task_input"> <button id="add_task"> Добавить </button></li>');
-        $("#add_task").click(function(){
+       $("ul").append('<input id="add_task_input"> <button id="add_task"> Добавить </button>');
+       $("#add_task").click(function(){
           var element = $('<li> <span></span> <button class="removing"> Удалить </button> </li>');
           $('span', element).text( $("#add_task_input").val());
           $("ul").append(element);
           $(".removing").click(function(){
         	$(this).parent().remove()
-          });
+        });
+
+
         });
 })
